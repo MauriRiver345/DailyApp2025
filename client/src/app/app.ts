b52be83dd0eab331f0dc77ad33e79e98a786abd1
MauriRiver345 <mrg2735@gmail.com>
2025-09-03 07:52:26 -0600

    feat: Add http client to SPA

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+  
+
+  ngOnInit(): void { 
+    this.http.get("http://localhost:5081/api/members").subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log("Request complete")
+
+      
+    })
+}
 }
