4b618935ecea73c6e7542be27018bf78def8dadc
MauriRiver345 <mrg2735@gmail.com>
2025-09-22 20:36:12 -0600

    feat: Parcial02 - Add JWT Middleware

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
-namespace API.Controllers
-{
+namespace API.Controllers;
+
+[Authorize]
     public class MembersController(AppDbContext context): BaseApiController
     {
         [HttpGet]
@@ -19,6 +21,7 @@ namespace API.Controllers
             return members;
         }
 
+        [AllowAnonymous]
         [HttpGet("{id}")] //https://localhost:5001/api/members/obed-id
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
@@ -27,4 +30,3 @@ namespace API.Controllers
             return member;
         }
     }
-}
--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,10 @@ using API.Data;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +17,19 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection"));
 });
 builder.Services.AddCors();
-builder.Services.AddSingleton<ITokenService, TokenService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var tokenKey = builder.Configuration["TokenKey"] ?? throw new ArgumentNullException("Cannot get the token key - Program.cs");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 
 var app = builder.Build();
