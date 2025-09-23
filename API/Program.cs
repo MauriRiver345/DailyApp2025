66098183ebadfd37c306a5902eaa5ceccd37cb32
MauriRiver345 <mrg2735@gmail.com>
2025-09-22 20:45:17 -0600

    feat: Parcial02 - Add JWT Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -34,8 +34,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
