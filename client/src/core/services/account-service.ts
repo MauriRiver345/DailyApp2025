7302f9f853c2cd14ea0642311bac52aae042fdbe
MauriRiver345 <mrg2735@gmail.com>
2025-09-23 19:43:33 -0600

    feat: Parcial02 - Add Account Service

---
+++ client/src/core/services/account-service.ts
@@ -0,0 +1,14 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:5001/api/';
+
+  login(creds: any){
+    return this.http.post(this.baseUrl + 'account/login', creds);
+  }
+}
