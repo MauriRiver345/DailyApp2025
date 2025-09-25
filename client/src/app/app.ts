82f7d2a2d472c9ff89ff23411736d4de4f9678e8
MauriRiver345 <mrg2735@gmail.com>
2025-09-25 07:31:39 -0600

    feat: Parcial02 - Add parent...

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -5,6 +5,7 @@ import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
 import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
@@ -12,31 +13,32 @@ import { Home } from "../features/home/home";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
+export class App implements OnInit {
   private accountService = inject(AccountService);
   private http = inject(HttpClient);
-  protected readonly title = signal('Dating App');
-  protected members = signal<any>([]);  
 
-  async ngOnInit(): Promise<void> { 
+  protected readonly title = signal('Dating App')
+  protected members = signal<User[]>([]);
+
+  async ngOnInit(): Promise<void> {
     this.setCurrentUser();
-    this.members.set(await this.getMembers());
+    this.members.set(await this.getMembers())
   }
 
-  setCurrentUser(): void{
+  setCurrentUser(): void {
     const userString = localStorage.getItem("user");
-    if(!userString) return;
+    if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
   }
 
-  async getMembers(): Promise<Object>{
+  async getMembers(): Promise<User[]> {
     try {
-      return lastValueFrom(this.http.get("https://localhost:5001/api/members"))
-    } catch (error){
-      console.log(error);
+      return lastValueFrom(this.http.get<User[]>("https://localhost:5001/api/members"))
+    }
+    catch (error) {
+      console.error(error);
       throw error;
     }
   }
-
 }
--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, Input, input, signal } from '@angular/core';
+import { Register } from '../register/register';
+import { User } from '../../types/user';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Register],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  @Input({ required: true }) usersFromApp!: User[];
+
   protected registerMode = signal(false);
 
   showRegister(): void {
