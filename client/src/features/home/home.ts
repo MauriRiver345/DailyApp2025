41970b4dddfa280277b0eb88c77986f9bdcf4a6d
MauriRiver345 <mrg2735@gmail.com>
2025-09-25 07:43:28 -0600

    feat: Add child to parent communication

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -12,7 +12,7 @@ export class Home {
 
   protected registerMode = signal(false);
 
-  showRegister(): void {
-    this.registerMode.set(true);
+  showRegister(value: boolean): void {
+    this.registerMode.set(value);
   }
 }
